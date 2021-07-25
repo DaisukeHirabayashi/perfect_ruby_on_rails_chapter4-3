7ebd5c642ab48e429f6d1bccf05a0b8ff955de4a
DaisukeHirabayashi <daisuke@pluslab.org>
2021-07-25 16:33:10 +0900

    イベントリスナーの追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+document.addEventListener("turbolinks:load", function(){
+    console.log("turbolinks:load fire");
+})
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
